0046bd6ef4f89ae8c9884459166d20c6351f951c
naman-mehta2001 <f20190981@pilani.bits-pilani.ac.in>
2021-02-19 21:47:40 +0530

    feat

--- action.js
+++ action.js
@@ -1,25 +1,48 @@
 var button = document.getElementById("input");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-
-function inputLength() {
-	return input.value.length;
+var listItem = document.querySelectorAll("li");
+var dlt = document.querySelectorAll(".delete")
+//Make delete button
+function delButton(parent) {
+	var del = document.createElement("button");
+	del.appendChild(document.createTextNode("Done!"));
+	del.className = "delete";
+	parent.appendChild(del);	
 }
-function addElement(){
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value="";
-}
-function onClicking(){
-    if(inputLength()>0){
-        addElement();
-    }
-}
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		addElement();
+//delete functionality ; specify [i] in all for loops!
+function deleteMe() {
+	for (var i = 0; i < dlt.length; i++) {
+	dlt[i].addEventListener("click", function() {
+		this.parentNode.remove();
+		})
 	}
 }
-button.addEventListener("click",onClicking);
-input.addEventListener("keypress", addListAfterKeypress);
+
+function addToggle() {
+	this.classList.toggle("done");
+}
+
+function addEntry() {
+	if (input.value.length > 0 ) {
+		var newItem = document.createElement("li");
+		newItem.appendChild(document.createTextNode(input.value));
+		ul.appendChild(newItem);	
+		newItem.addEventListener("click",addToggle);
+		//delete
+		delButton(newItem);
+		dlt = document.querySelectorAll(".delete");
+		deleteMe();
+			input.value="";	}
+}
+
+button.addEventListener("click", addEntry);
+input.addEventListener("keypress", function() {
+	if ( event.keyCode === 13 ) {
+	addEntry()}
+});
+
+for ( var i = 0 ; i < listItem.length; i++) {
+ 	listItem[i].addEventListener("click", addToggle);
+}
+
